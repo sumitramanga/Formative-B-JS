72bc582a994eb785e146ba3b20f9b9cf4f7b39fc
sumitra manga <sumitramanga@gmail.com>
2018-06-01 11:51:09 +1200

    Created variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
-(function() {
-  'use strict';
+(function() { // IIFE Begins
+  var getImageOption = document.getElementById('image');
+  var getBgOption = document.getElementById('background');
+  var getFontOption = document.getElementById('font');
+  var getBorderOption = document.getElementById('border');
+  var getMessage = document.getElementById('message');
 
-}());
+  var getButton = document.querySelector('pure-button');
+
+  var storageArray = [];
+
+  console.dir(getBgOption);
+
+  $('#message').click(function(){
+
+    // storageArray.push(getBgOption);
+
+  });
+}()); // IIFE Ends
