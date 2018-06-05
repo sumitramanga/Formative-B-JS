383aa22247a60b1ff4230f805cce323a56ef2c62
sumitra manga <sumitramanga@gmail.com>
2018-06-05 13:02:00 +1200

    img src is working. Firguring out how to empty array and replace a class list item

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,12 @@
   var getBorderOption = document.getElementById('border');
   var getMessageOption = document.getElementById('message');
 
+  // Grabbing the form button
   var getButton = document.querySelector('pure-button');
 
   // Grabbing sections of the card that is already dispalyed on the page
   var getCard = document.querySelector('.card');
-  var getImage = document.getElementsByTagName('img');
+  var getImage = document.querySelector('img');
   var getBorder = document.querySelector('.noneBorder');
   var getMessage =  document.querySelector('.sketch');
 
@@ -21,22 +22,24 @@
 
   $('#border').click(function(){
 
+    // Pushing all the values from the form into an array
     storageArray.push(getImageOption.value, getBgOption.value, getFontOption.value, getBorderOption.value, getMessageOption.value);
 
-    // console.log(storageArray);
+    console.dir(getCard);
 
     // Creating the card using he users options. This is done by grabbing the
-    // storage array length
+    // storage array length.
+
+    getImage.src = 'assets/' + storageArray[0] + '.jpg';
     getCard.classList.add(storageArray[1] + 'Background');
     getMessage.className = storageArray[2];
     getBorder.classList.add(storageArray[3] + 'Border');
     getMessage.textContent = storageArray[4];
 
-    //
     // pop entire array
     // for (var i = 0; i < storageArray.length; i--) {
     //   storageArray.pop();
     // }
 
-  });
+  }); // Click function ends
 }()); // IIFE Ends
