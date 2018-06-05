8cb3b9322c4bfa1e5358a58b6957e1f084a85738
sumitra manga <sumitramanga@gmail.com>
2018-06-05 14:55:09 +1200

    Array empties after completing tasks. if else statement created to remove classes the user has submitted into the DOM.

--- assets/script.js
+++ assets/script.js
@@ -20,12 +20,12 @@
 
   var storageArray = [];
 
-  $('#border').click(function(){
+  $('#message').click(function(){
 
     // Pushing all the values from the form into an array
     storageArray.push(getImageOption.value, getBgOption.value, getFontOption.value, getBorderOption.value, getMessageOption.value);
 
-    console.dir(getCard);
+    console.log(storageArray);
 
     // Creating the card using he users options. This is done by grabbing the
     // storage array length.
@@ -33,13 +33,40 @@
     getImage.src = 'assets/' + storageArray[0] + '.jpg';
     getCard.classList.add(storageArray[1] + 'Background');
     getMessage.className = storageArray[2];
-    getBorder.classList.add(storageArray[3] + 'Border');
     getMessage.textContent = storageArray[4];
 
-    // pop entire array
-    // for (var i = 0; i < storageArray.length; i--) {
+
+    // This was a previous option used when changing the values in the HTML to
+    // according the border option name (<option value="smalleBorder">Thin</option>)
+    // getBorder.classList.add(storageArray[3] + 'Border');
+
+
+    // Old method
+    // for (var i = storageArray.length; i > 0; i--) {
     //   storageArray.pop();
     // }
 
+
+    // Adds or removes border. Remove method is used to to remove previous classes
+    // the user have submitted to the DOM
+    if (getBorderOption.value === 'none') {
+        getBorder.classList.remove('smallBorder', 'bigBorder', 'filledBorder');
+        getBorder.classList.add('noneBorder');
+
+    } else if (getBorderOption.value === 'thin') {
+      getBorder.classList.remove('noneBorder', 'bigBorder', 'filledBorder');
+      getBorder.classList.add('smallBorder');
+
+    } else if(getBorderOption.value === 'thick') {
+      getBorder.classList.remove('smallBorder', 'noneBorder', 'filledBorder');
+      getBorder.classList.add('bigBorder');
+
+    } else {
+      getBorder.classList.remove('smallBorder', 'bigBorder', 'noneBorder');
+      getBorder.classList.add('filledBorder');
+    }
+
+    storageArray.length = 0;
+    console.log(storageArray);
   }); // Click function ends
 }()); // IIFE Ends
